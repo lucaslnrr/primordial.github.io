cc81de4e892acc34f4311f4cc06b53f4ca8ee935
lucaslnrr <lucas.lnrr@gmail.com>
2025-08-31 22:04:50 -0300

    Update footer design with professional dark style

--- js/script.js
+++ js/script.js
@@ -11,23 +11,26 @@
         autoplay: true,
         autoplaySpeed: 2e3,
     });
-    $(".clients-logo").slick({
-        infinite: true,
-        arrows: false,
-        autoplay: true,
-        slidesToShow: 6,
-        slidesToScroll: 6,
-        autoplaySpeed: 6e3,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: { slidesToShow: 6, slidesToScroll: 6, infinite: true, dots: true },
-            },
-            { breakpoint: 900, settings: { slidesToShow: 4, slidesToScroll: 4 } },
-            { breakpoint: 600, settings: { slidesToShow: 4, slidesToScroll: 4 } },
-            { breakpoint: 480, settings: { slidesToShow: 2, slidesToScroll: 2 } },
-        ],
-    });
+$(".clients-logo").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    speed: 3000,          // controla a velocidade do movimento (maior = mais lento)
+    autoplaySpeed: 0,     // inicia imediatamente, sem pausa
+    cssEase: "linear",    // movimento contínuo
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: { slidesToShow: 6, slidesToScroll: 1, infinite: true }
+        },
+        { breakpoint: 900, settings: { slidesToShow: 4, slidesToScroll: 1 } },
+        { breakpoint: 600, settings: { slidesToShow: 3, slidesToScroll: 1 } },
+        { breakpoint: 480, settings: { slidesToShow: 2, slidesToScroll: 1 } }
+    ],
+});
+
     $(".testimonial-wrap").slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -110,3 +113,68 @@
         }
     });
 })(jQuery);
+// Banner background carousel
+// Banner background carousel (5s por slide)
+
+// Patch slides that use --bg:url(...) to real background-image
+(function () {
+  document.querySelectorAll('.banner-slider .slide').forEach(function (slide) {
+    var raw = slide.getAttribute('style') || '';
+    var m = raw.match(/--bg:\s*url\((["']?)(.*?)\1\)/i);
+    if (m && m[2]) {
+      slide.style.backgroundImage = 'url(' + m[2] + ')';
+      // keep other inline styles intact
+      // console.log('Set slide bg:', m[2]);
+    }
+  });
+})();
+$(function(){
+
+  var $slider = $('.banner-slider');
+
+  $slider.on('init', function(event, slick){
+    controlAutoplay(slick, slick.currentSlide);
+  });
+
+  $slider.on('afterChange', function(event, slick, currentSlide){
+    controlAutoplay(slick, currentSlide);
+  });
+
+  $slider.slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 7000,   // default, but we'll override manually
+    speed: 700,
+    fade: true,
+    cssEase: 'linear',
+    pauseOnHover: false,
+    adaptiveHeight: false
+  });
+
+  var timer;
+
+  function controlAutoplay(slick, index){
+    clearTimeout(timer);
+
+    var $cur = $(slick.$slides[index]);
+    var isVideo = $cur.hasClass('video-slide');
+
+    // pause Slick's internal autoplay
+    slick.slickPause();
+
+    // play video if exists
+    $cur.find('video').each(function(){
+      this.currentTime = 0;
+      this.play && this.play();
+    });
+
+    // choose duration
+    var delay = isVideo ? 12000 : 7000;
+
+    timer = setTimeout(function(){
+      slick.slickNext();
+    }, delay);
+  }
+
+});
